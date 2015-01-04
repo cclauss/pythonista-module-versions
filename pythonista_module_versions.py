bc9813e7a2906b40b0765029c22d9e528d21f3d3
cclauss <cclauss@bluewin.ch>
2015-01-04 12:31:21 +0100

    Added mechanize tuple workaround

--- pythonista_module_versions.py
+++ pythonista_module_versions.py
@@ -51,7 +51,7 @@ html5lib   0.999   v.s. 0.999   current version
 ### hasattr(markdown, 'version')
 markdown   2.2.0   v.s. 2.5.2   OLDER VERSION
 markdown2  2.2.1   v.s. 2.3.0   OLDER VERSION
-mechanize  (0, 2, 5, None, None) v.s. 0.2.5  current version
+mechanize  0.2.5   v.s. 0.2.5  current version
 paramiko   1.13.0  v.s. 1.15.2  OLDER VERSION
 PIL        ?????   v.s. 1.1.6   unclear
 pygments   1.6     v.s. 2.0.1   OLDER VERSION
