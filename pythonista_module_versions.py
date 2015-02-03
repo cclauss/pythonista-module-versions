827a5e48b95522ff74b7cf14e7f5d91024acb729
cclauss <cclauss@bluewin.ch>
2015-02-03 02:14:16 +0100

    Update pythonista_module_versions.py

--- pythonista_module_versions.py
+++ pythonista_module_versions.py
@@ -2,7 +2,8 @@ import bs4, importlib, requests  #, pkgutil
 
 pypi_dict = { 'bs4'      : 'beautifulsoup4',
               'dateutil' : 'py-dateutil',
-              'faker'    : 'Faker' }
+              'faker'    : 'Faker',
+              'yaml'     : 'PyYAML }
 
 def get_module_version(in_module_name = 'requests'):
     mod = importlib.import_module(in_module_name)
@@ -37,9 +38,10 @@ print(fmt.format('module', 'local', 'PyPI'))
 print(fmt.format('name', 'version', 'version'))
 div = fmt.format('-' * 10, '-' * 7, '-' * 7)
 print(div)
-modules = '''bottle bs4 dateutil dropbox ecdsa evernote faker feedparser flask
-             html5lib markdown markdown2 matplotlib mechanize numpy paramiko PIL
-             pyflakes pygments pyparsing requests six werkzeug wsgiref xmltodict'''
+modules = '''bottle bs4 cffi ctypes dateutil dropbox ecdsa evernote faker feedparser flask html5lib
+             itsdangerous jedi jinja2 markdown markdown2 matplotlib mechanize numpy oauth2 paramiko
+             PIL pyflakes pygments pyparsing PyPDF2 pytz qrcode reportlab requests simpy six
+             sqlalchemy sympy  werkzeug wsgiref xhtml2pdf xmltodict yaml'''
 for module_name in modules.split():
     local_version = get_module_version(module_name)
     pypi_version  = get_module_version_from_pypi(module_name)
