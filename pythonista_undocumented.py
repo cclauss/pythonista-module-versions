d892f64413c25c04ab2571519622c83e239f876a
cclauss <cclauss@bluewin.ch>
2015-04-25 16:10:23 +0200

    Modules added in Pythonista v1.6 Beta
    modules += '''appex cb ctypes dialogs sk'''.split()  # modules added in Pythonista v1.6 Beta

--- pythonista_undocumented.py
+++ pythonista_undocumented.py
@@ -1,7 +1,9 @@
-Aimport bs4, importlib, inspect, requests, itertools
+# coding: utf-8
+import bs4, importlib, inspect, requests, itertools
 
 modules = '''canvas clipboard console contacts editor keychain linguistictagger
              location motion notification photos scene sound speech ui'''.split()
+modules += '''appex cb ctypes dialogs sk'''.split()  # modules added in Pythonista v1.6 Beta
 
 def inspect_functions(module_name):
     module = importlib.import_module(module_name)
@@ -25,37 +27,6 @@ def undocumented_functions(module_name):
                                            if f not in website_funcs]
 
 undocumented = [x for x in [undocumented_functions(module_name)
-                                 for module_name in modules]]
+                                 for module_name in sorted(modules)]]
 print('=' * 40)
 print('\n'.join(itertools.chain(*undocumented)))  # flatten()
-
-'''
-keychain.get_services()
-motion.MotionUpdates()
-notification.set_badge()
-scene.Button()
-scene.Number()
-scene.TextLayer()
-scene.curve_ease_back_in()
-scene.curve_ease_back_in_out()
-scene.curve_ease_back_out()
-scene.dedent()
-scene.iskeyword()
-scene.recordtype()
-scene.size()
-scene.sqrt()
-sound.Player()
-speech.get_languages()
-speech.is_speaking()
-ui.ActivityIndicator()
-ui.ListDataSourceList()
-ui.add_observer()
-ui.begin_image_context()
-ui.close_all()
-ui.end_editing()
-ui.end_image_context()
-ui.get_keyboard_frame()
-ui.remove_all_observers()
-ui.remove_observer()
-ui.set_alpha()
-'''
