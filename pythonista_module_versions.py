546bfbb4b7039b30429b88e1003b9df9525a3bf0
cclauss <cclauss@bluewin.ch>
2015-05-01 10:00:38 +0200

    Remove cffi

--- pythonista_module_versions.py
+++ pythonista_module_versions.py
@@ -8,7 +8,7 @@ pypi_dict = { 'bs4'      : 'beautifulsoup4',
               'yaml'     : 'PyYAML',
               'Crypto'   : 'pycrypto'}
 
-modules = '''bottle bs4 cffi Crypto ctypes dateutil dropbox ecdsa evernote faker feedparser flask
+modules = '''bottle bs4 Crypto ctypes dateutil dropbox ecdsa evernote faker feedparser flask
              html2text html5lib httplib2 itsdangerous jedi jinja2 markdown markdown2 matplotlib
              mechanize mpmath numpy oauth2 paramiko parsedatetime PIL pycparser pyflakes pygments
              pyparsing PyPDF2 pytz qrcode reportlab requests simpy six sqlalchemy sympy thrift
