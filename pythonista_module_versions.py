3e27b35fa370abfcbe1205ca5553151f27ed46f2
cclauss <cclauss@bluewin.ch>
2015-05-01 11:40:10 +0200

    Iodates thé output at the bottom

--- pythonista_module_versions.py
+++ pythonista_module_versions.py
@@ -109,33 +109,58 @@ requests   2.2.1   v.s. 2.5.1   OLDER VERSION
 six        1.6.1   v.s. 1.9.0   OLDER VERSION
 wsgiref    ?????   v.s. 0.1.2   unclear
 xmltodict  0.8.7   v.s. 0.9.0   OLDER VERSION
-==================================
-| module     | local   | PyPI    |
-| name       | version | version |
-| ---------- | ------- | ------- |
-| bottle     | 0.12.5  | 0.12.8  |
-| bs4        | 4.3.2   | 4.3.2   |
-| dateutil   | 1.5-mpl | 2.2     |
-| dropbox    | ?????   | 2.2.0   |
-| ecdsa      | 0.11    | 0.11    |
-| evernote   | ?????   | 1.25.0  |
-| faker      | ?????   | 0.0.4   |
-| feedparser | 5.1.3   | 5.1.3   |
-| flask      | 0.10.1  | 0.10.1  |
-| html5lib   | 0.999   | 0.999   |
+```
+| module        | local    | PyPI       |
+| name          | version  | version    |
+| ------------- | -------- | ---------- |
+| bottle        | 0.12.5   | 0.12.8     |
+| bs4           | 4.3.2    | 4.3.2      |
+| Crypto        | 2.6      | 2.6.1      |
+| ctypes        | 1.1.0    | 1.0.2      |
+| dateutil      | 1.5-mpl  | 2.2        |
+| dropbox       | ?????    | 2.2.0      |
+| ecdsa         | 0.11     | 0.13       |
+| evernote      | ?????    | 1.25.0     |
+| faker         | ?????    | 0.0.4      |
+| feedparser    | 5.1.3    | 5.2.0      |
+| flask         | 0.10.1   | 0.10.1     |
+| html2text     | 2014.4.5 | 2015.4.14  |
+| html5lib      | 0.999    | 0.99999    |
+| httplib2      | 0.8      | 0.9.1      |
+| itsdangerous  | ?????    | 0.24       |
+| jedi          | 0.9.0    | 0.9.0      |
+| jinja2        | 2.7      | 2.7.3      |
 ### hasattr(markdown, 'version')
-| markdown   | 2.2.0   | 2.5.2   |
-| markdown2  | 2.2.1   | 2.3.0   |
-| mechanize  | 0.2.5   | 0.2.5   |
-| paramiko   | 1.13.0  | 1.15.2  |
-| PIL        | ?????   | 1.1.6   |
-| pyflakes   | 0.7.3   | 0.8.1   |
-| pygments   | 1.6     | 2.0.2   |
-| pyparsing  | 2.0.1   | 2.0.3   |
-| requests   | 2.2.1   | 2.5.1   |
-| six        | 1.6.1   | 1.9.0   |
-| werkzeug   | 0.9.4   | 0.9.6   |
-| wsgiref    | ?????   | 0.1.2   |
-| xmltodict  | 0.8.7   | 0.9.1   |
-| ---------- | ------- | ------- |
+| markdown      | 2.2.0    | 2.6.2      |
+| markdown2     | 2.2.1    | 2.3.0      |
+| matplotlib    | 1.3.1    | 1.4.3      |
+| mechanize     | 0.2.5    | 0.2.5      |
+| mpmath        | 0.18     | 0.19       |
+| numpy         | 1.8.0    | 1.9.2      |
+| oauth2        | 1.5.211  | 1.5.211    |
+| paramiko      | 1.13.0   | 1.15.2     |
+| parsedatetime | 1.3      | 1.4        |
+| PIL           | ?????    | 1.1.6      |
+| pycparser     | 2.10     | 2.12       |
+| pyflakes      | 0.7.3    | 0.8.1      |
+| pygments      | 1.6      | 2.0.2      |
+| pyparsing     | 2.0.1    | 2.0.3      |
+| PyPDF2        | 1.22     | 1.24       |
+| pytz          | 2013b    | 2015.2     |
+| qrcode        | ?????    | 5.1        |
+| reportlab     |  $Id$    | 3.1.44     |
+| requests      | 2.6.0    | 2.6.2      |
+| simpy         | 3.0.2    | 3.0.7      |
+| six           | 1.6.1    | 1.9.0      |
+| sqlalchemy    | 0.9.7    | 1.0.3      |
+| sympy         | 0.7.4.1  | 0.7.6      |
+| thrift        | ?????    | 0.9.2      |
+| werkzeug      | 0.9.4    | 0.10.4     |
+| wsgiref       | ?????    | 0.1.2      |
+| xhtml2pdf     | 3.0.33   | 0.0.6      |
+| xmltodict     | 0.8.7    | 0.9.2      |
+| yaml          | 3.09     | 3.11       |
+| ------------- | -------- | ---------- |
+```
+================
 '''
